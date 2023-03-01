fac1a0414c0136296e6ffa198dde4002c765df39
Luiz Felipe dos Santos <lz18.felipe@gmail.com>
2023-03-01 18:49:29 -0300

    cumprindo terceiro requisito do exercício

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-2-javascript-array-e-loop-for/lidando-com-arrays.js
@@ -0,0 +1,29 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let media = 0;
+let soma = 0;
+let maior = numbers[0]
+
+/* for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index]);
+    soma += numbers[index];
+    media = soma / numbers.length;
+}
+
+console.log('A dos valores é: ' + soma);
+console.log('A média dos valores é: ' + media);
+
+if (media > 20) {
+    console.log('Valor maior que 20');
+} else {
+    console.log('Valor menor que 20');
+}  */
+
+/* for (index = 1; index < numbers.length; index += 1) {
+    if (numbers[index] > maior) {
+        maior = numbers[index];
+    }
+} 
+
+console.log(maior); */
+
+for ()
