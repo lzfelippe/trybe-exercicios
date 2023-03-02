2d60cb0c5fcf96d6d2522404889bfca4c6d316c6
Luiz Felipe dos Santos <lz18.felipe@gmail.com>
2023-03-02 07:16:35 -0300

    completo requisito descobrir numeros impares do array

--- fundamentos/secao-03-introducao-a-javascript/dia-2-javascript-array-e-loop-for/lidando-com-arrays.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-2-javascript-array-e-loop-for/lidando-com-arrays.js
@@ -2,6 +2,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let media = 0;
 let soma = 0;
 let maior = numbers[0]
+let contImpar = 0;
 
 /* for (let index = 0; index < numbers.length; index += 1) {
     console.log(numbers[index]);
@@ -26,4 +27,17 @@ if (media > 20) {
 
 console.log(maior); */
 
-for ()
+/* for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 !== 0) {
+        contImpar += 1;
+    }
+}
+
+if (contImpar === 0) {
+    console.log('Nenhum valor ímpar encontrado');
+} else {   
+    console.log(`São ${contImpar} números ÍMPARES`);
+}
+ */
+
+
