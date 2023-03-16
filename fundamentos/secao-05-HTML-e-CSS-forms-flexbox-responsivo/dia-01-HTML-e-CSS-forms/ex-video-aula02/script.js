d57f3718f2fae35b25f8684fa6a315818483c5c1
Luiz Felipe dos Santos <lz18.felipe@gmail.com>
2023-03-16 20:38:41 -0300

    concluindo requisito 1

--- fundamentos/secao-05-HTML-e-CSS-forms-flexbox-responsivo/dia-01-HTML-e-CSS-forms/ex-video-aula02/script.js
+++ fundamentos/secao-05-HTML-e-CSS-forms-flexbox-responsivo/dia-01-HTML-e-CSS-forms/ex-video-aula02/script.js
@@ -7,13 +7,28 @@ const HREF_LINK = document.querySelector("#href");
 //Na função do evento, implemente o método .preventDefault() para prevenir
 //o comportamento padrão do elemento. Ou seja, ao clicar no link, nada deve acontecer.
 
-
+function noOpenNewPage(event) {
+    event.preventDefault();
+    console.log(HREF_LINK.value);
+}
+HREF_LINK.addEventListener('click', noOpenNewPage);
 
 //Adicione um evento click no elemento checkbox que possui o id input-checkbox.
 //Na função do evento, implemente o método .preventDefault() para prevenir o comportamento
 // padrão do elemento. Ou seja, ao clicar na caixinha, nada deve acontecer.
 
+function noCheckBox (event) {
+    event.preventDefault();
+    console.log(INPUT_CHECKBOX.value);
+}
+INPUT_CHECKBOX.addEventListener('click', noCheckBox);
+
 //Adicione um evento keypress no elemento input type="text" que possui o id input-text.
 // Na função do evento, implemente o método .preventDefault() de forma que somente
 // o caractere a (letra ‘a’ minúscula) possa ser digitado na caixinha. 
 //(dica: para capturar a tecla digitada, utilize o event.key)
+
+function exclusiveAceptA (event.keypress) {
+    if (keypress === 'a')
+    event.preventDefault();
+}
