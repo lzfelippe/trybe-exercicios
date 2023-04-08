aa3125aa1ebe79e06cceceec9113863c5da4d724
Luiz Felipe dos Santos <lz18.felipe@gmail.com>
2023-04-08 17:45:18 -0300

    finalizando testes e praFixar

--- fundamentos/secao-07-higher-order-functions-do-javascript-ES6/dia-01-introducao-a-higher-order-functions/testes.js
+++ fundamentos/secao-07-higher-order-functions-do-javascript-ES6/dia-01-introducao-a-higher-order-functions/testes.js
@@ -37,16 +37,16 @@
 
 // Array.SOME()
 
-// const pessoas = [
-//     { nome: 'Ana', cargo: 'Analista' },
-//     { nome: 'João', cargo: 'Gerência' },
-//     { nome: 'Aline', cargo: 'Analista' },
-//     { nome: 'Joana', cargo: 'Gerência' },
-//   ];
+const pessoas = [
+    { nome: 'Ana', cargo: 'Analista' },
+    { nome: 'João', cargo: 'Gerência' },
+    { nome: 'Aline', cargo: 'Analista' },
+    { nome: 'Joana', cargo: 'Gerência' },
+  ];
   
-//   const verificaCargo = pessoas.some((pessoa) => pessoa.cargo === 'Product Owner');
+  const verificaCargo = pessoas.some((pessoa) => pessoa.cargo === 'Product Owner');
   
-//   console.log(verificaCargo); // false
+  console.log(verificaCargo); // false
 
 const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
 
@@ -54,4 +54,6 @@ const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === le
 
 console.log(verifyFirstLetter('J', listNames)); // true
 console.log(verifyFirstLetter('X', listNames)); // false
+
+// Array.EVERY()
 
