379d6cfd39a3beb167b5b411bd351ceadbeb548d
Luiz Felipe dos Santos <lz18.felipe@gmail.com>
2023-05-13 18:46:25 -0300

    fix: corrige exercicio_01

---
+++ front-end/secao-04-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/exercise-01/src/App.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import PersonDetails from './components/PersonDetails';
+import './App.css'
+
+
+class App extends Component {
+
+    render() {
+      return (
+        <div>
+          <PersonDetails />
+        </div>
+      );
+    }
+}
+
+export default App;
