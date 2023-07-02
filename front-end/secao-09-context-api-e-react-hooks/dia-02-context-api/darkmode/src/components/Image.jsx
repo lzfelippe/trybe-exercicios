3cba910cca3f64106237226c59015701650125fa
Luiz Felipe dos Santos <lz18.felipe@gmail.com>
2023-07-02 01:09:36 -0300

    feat: finaliza projeto darkmode contextApi

---
+++ front-end/secao-09-context-api-e-react-hooks/dia-02-context-api/darkmode/src/components/Image.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../logo.svg';
+
+function Image() {
+  return (
+    <div>
+        <img src={ logo } alt="logo-trybe" />
+    </div>
+  )
+}
+
+export default Image
